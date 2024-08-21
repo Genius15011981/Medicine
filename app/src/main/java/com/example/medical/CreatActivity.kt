842eeeeac98a44be3d8d82fa8aa9f978e00caab1
Golibjon Odinaev <109343678+Geniy1@users.noreply.github.com>
2024-08-21 18:05:56 +0500

    21.08.2024 18:03

--- app/src/main/java/com/example/medical/CreatActivity.kt
+++ app/src/main/java/com/example/medical/CreatActivity.kt
@@ -1,6 +1,7 @@
 package com.example.medical
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,4 +12,6 @@ class CreatActivity : AppCompatActivity() {
         setContentView(R.layout.activity_creat)
 
     }
+
+    fun next(view: View) {}
 }
